aedcb564d881ece2f5bc6335cb639434242dd451
Denny <denny1011133@luci.group>
2022-02-14 10:47:07 +0800

    setUp reducers and AlbumList component

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AlbumList from './AlbumList';
+
+const App = () => {
+  return (
+    <div className="ui container">
+      <AlbumList />
+    </div>
+  );
+};
+
+export default App;
